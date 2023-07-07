0279674f0987e73aa3e0c93adfefee5962969d11
Cristian <89285428+Cristiangonzz@users.noreply.github.com>
2023-07-07 18:51:07 -0300

    se agrego un nuevo archivo de presentacion

--- src/app/presentation/home/home.module.ts
+++ src/app/presentation/home/home.module.ts
@@ -4,15 +4,16 @@ import { SharedModule } from '../shared/shared.module';
 import { HomeComponent } from './home/home.component';
 import { RoutingHomeModule } from './routing-home.module';
 import { CursoModule } from '../course/curso.module';
+import { PresentacionComponent } from './presentacion/presentacion.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent,PresentacionComponent],
   imports: [
     CommonModule,
     RoutingHomeModule,
     SharedModule,
     CursoModule,
   ],
-  exports: [],
+  exports: [PresentacionComponent],
 })
 export class HomeModule {}
